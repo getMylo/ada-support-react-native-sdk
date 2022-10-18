3f78e79044b4789ead2b508e41ebfe178b3d7181
Mitch Aldrich <mitch.aldrich@mogo.ca>
2022-10-18 10:42:48 -0400

    update to use react-native-blob-util

--- react_native_pods.rb
+++ react_native_pods.rb
@@ -1,7 +1,7 @@
 def use_ada! (options={})
   # The prefix to react-native
-  prefix = options[:path] ||= "../node_modules/"
+  prefix = options[:path] ||= "../node_modules"
 
   # The Pods which should be included in all projects
-  pod 'rn-fetch-blob', :path => "#{prefix}/rn-fetch-blob"
+  pod 'react-native-blob-util', :path => "#{prefix}/react-native-blob-util"
 end
