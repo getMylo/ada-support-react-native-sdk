c40f3e1af08bb95e710ebc7484b5eff12c540519
Mitch Aldrich <mitch.aldrich@mogo.ca>
2022-10-18 09:30:09 -0400

    fix path

--- react_native_pods.rb
+++ react_native_pods.rb
@@ -1,6 +1,6 @@
 def use_ada! (options={})
   # The prefix to react-native
-  prefix = options[:path] ||= "../node_modules/"
+  prefix = options[:path] ||= "../node_modules"
 
   # The Pods which should be included in all projects
   pod 'react-native-blob-util', :path => "#{prefix}/react-native-blob-util"
